86a8f1692fe51ec9b7d8e49f4064a07206ed4bf9
John Kenedy Neves <77014157+johnkenedy@users.noreply.github.com>
2022-12-20 22:11:26 -0300

    Feat/activity goal screen (#6)
    * WelcomeScreen and compositionLocalOf()
    
    * Add NavControllerExt and UiEvent
    
    * remove unnecessary classpath
    
    * SharedPreferences Setup
    
    * OnBoarding Components - SelectableButton and UnitTextField
    
    * GenderScreen and GenderViewModel
    
    * AgeScreen and AgeViewModel
    
    * HeightScreen/ViewModel and WeightScreen/ViewModel
    
    * ActivityScreen/ViewModel and GoalScreen/ViewModel
    
    Co-authored-by: john kenedy <john.kenedy232@gmail.com>

--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.core.navigation.Route
+import com.example.onboarding_presentation.activity.ActivityScreen
 import com.example.onboarding_presentation.age.AgeScreen
 import com.example.onboarding_presentation.gender.GenderScreen
+import com.example.onboarding_presentation.goal.GoalScreen
 import com.example.onboarding_presentation.height.HeightScreen
 import com.example.onboarding_presentation.weight.WeightScreen
 import com.example.onboarding_presentation.welcome.WelcomeScreen
@@ -64,10 +66,10 @@ class MainActivity : ComponentActivity() {
 
                         }
                         composable(Route.ACTIVITY) {
-
+                            ActivityScreen(onNavigate = navController::navigate)
                         }
                         composable(Route.GOAL) {
-
+                            GoalScreen(onNavigate = navController::navigate)
                         }
                         composable(Route.TRACKER_OVERVIEW) {
 
