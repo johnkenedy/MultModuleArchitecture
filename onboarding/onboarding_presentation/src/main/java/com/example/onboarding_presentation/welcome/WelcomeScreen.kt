10b0911eabfec5bab1369bc626a3450248a32adc
John Kenedy Neves <77014157+johnkenedy@users.noreply.github.com>
2022-12-20 22:00:56 -0300

    Feat/navigation util (#2)
    * WelcomeScreen and compositionLocalOf()
    
    * Add NavControllerExt and UiEvent
    
    * remove unnecessary classpath
    
    * GenderScreen and GenderViewModel
    
    * Project_Default.xml
    
    Co-authored-by: john kenedy <john.kenedy232@gmail.com>

--- onboarding/onboarding_presentation/src/main/java/com/example/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/example/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -11,16 +11,18 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
+import com.example.core.navigation.Route
+import com.example.core.util.UiEvent
 import com.example.core_ui.LocalSpacing
 import com.example.onboarding_presentation.components.ActionButton
 import com.plcoding.core.R
 
 @Composable
-fun WelcomeScreen() {
+fun WelcomeScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit
+) {
     val spacing = LocalSpacing.current
     Column(
         modifier = Modifier
@@ -37,7 +39,7 @@ fun WelcomeScreen() {
         Spacer(modifier = Modifier.height(spacing.spaceMedium))
         ActionButton(
             text = stringResource(id = R.string.next),
-            onClick = { /*TODO*/ },
+            onClick = { onNavigate(UiEvent.Navigate(Route.AGE)) },
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
     }
